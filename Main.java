18a3c9f7e7bf8753b114fb2679d443a0866b4f59
DerekZhou <derekzhou2003@gmail.com>
2017-10-30 16:04:22 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import hsa.Console;
+
+public class Main
+{
+    static Console c;
+    
+    public static void main (String[] args)
+    {
+	c = new Console ();
+	
+	c.println("Hello World!");
+    }
+}
